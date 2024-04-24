06cfd9160b944722075581d0fcc8003d5333cff8
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-04-24 16:43:28 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Header from './Header';
 import About from './About';
 import ArticleList from './ArticleList';
-import blog from '../data/blog';
+import blogData from '../data/blog';
 
 const App = () => {
   return (
